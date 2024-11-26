d98dda99caed51133e502fa27d6875f431001f01
erikurfo <anna.s.fisenko@gmail.com>
2024-11-26 14:05:27 +0700

    it works!

--- main.py
+++ main.py
@@ -10,16 +10,37 @@ class Crawler:
     def __init__(self, dbFileName):
         self.dbFileName = dbFileName
         self.conn = sqlite3.connect(self.dbFileName)
-        # self.initDB()
+        self.initDB()
 
     # 0. Деструктор
     def __del__(self):
         self.conn.close()
-        print("fin")
+        print('fin')
 
     # Индексирование одной страницы
     def addToIndex(self, soup, url):
-        pass
+        cursor = self.conn.cursor()
+        link_rowid = self.getEntryId('URLList', 'URL', url)
+
+        listOfUnwantedItems = ['script', 'style']
+        for script in soup.find_all(listOfUnwantedItems):
+            script.decompose()
+        text = soup.get_text()
+
+        words = self.separateWords(text)
+
+        word_location = 0
+        for word in words:
+            word_rowid = self.getEntryId('wordList', 'word', word)
+            if not word_rowid: 
+                # Реализовать isFiltered для третьей колонки
+                cursor.execute('INSERT INTO wordList VALUES (?, ?, ?)', (None, word, 0))
+                word_rowid = cursor.lastrowid
+            cursor.execute('INSERT INTO wordLocation VALUES (?, ?, ?, ?)', 
+                           (None, word_rowid, link_rowid, word_location))
+            word_location += 1
+        self.conn.commit()
+
 
     # Разбиение текста на слова
     # Функция возвращает список слов
@@ -34,47 +55,47 @@ class Crawler:
     # Инициализация таблиц в БД
     def initDB(self):
         cursor = self.conn.cursor()
-        cursor.execute("""DROP TABLE IF EXISTS wordlist;""")
-        cursor.execute("""DROP TABLE IF EXISTS URLList;""")
-        cursor.execute("""DROP TABLE IF EXISTS wordLocation;""")
-        cursor.execute("""DROP TABLE IF EXISTS linkBetweenURL;""")
-        cursor.execute("""DROP TABLE IF EXISTS linkWord;""")
+        cursor.execute('DROP TABLE IF EXISTS wordlist;')
+        cursor.execute('DROP TABLE IF EXISTS URLList;')
+        cursor.execute('DROP TABLE IF EXISTS wordLocation;')
+        cursor.execute('DROP TABLE IF EXISTS linkBetweenURL;')
+        cursor.execute('DROP TABLE IF EXISTS linkWord;')
 
-        cursor.execute("""CREATE TABLE IF NOT EXISTS wordlist  (
+        cursor.execute('''CREATE TABLE IF NOT EXISTS wordList  (
                                     rowid INTEGER PRIMARY KEY AUTOINCREMENT,
                                     word TEXT NOT NULL,
                                     isFiltred INTEGER NOT NULL
-                ); """
+                ); '''
             )
-        cursor.execute("""CREATE TABLE IF NOT EXISTS URLList  (
+        cursor.execute('''CREATE TABLE IF NOT EXISTS URLList  (
                                     rowid INTEGER PRIMARY KEY AUTOINCREMENT,
                                     URL TEXT NOT NULL
-                ); """
+                ); '''
             )
-        cursor.execute("""CREATE TABLE IF NOT EXISTS wordLocation  (
+        cursor.execute('''CREATE TABLE IF NOT EXISTS wordLocation  (
                                     rowid INTEGER PRIMARY KEY AUTOINCREMENT,
                                     fk_wordId INTEGER NOT NULL,
                                     fk_URLId INTEGER NOT NULL,
                                     location INTEGER NOT NULL,
                                     FOREIGN KEY (fk_wordId) REFERENCES wordList(rowId),
                                     FOREIGN KEY (fk_URLId) REFERENCES URLList(rowId)
-                ); """
+                ); '''
             )
-        cursor.execute("""CREATE TABLE IF NOT EXISTS linkBetweenURL  (
+        cursor.execute('''CREATE TABLE IF NOT EXISTS linkBetweenURL  (
                                     rowid INTEGER PRIMARY KEY AUTOINCREMENT,
                                     fk_FromURL_Id INTEGER NOT NULL,
                                     fk_ToURLId INTEGER NOT NULL,
                                     FOREIGN KEY (fk_FromURL_Id) REFERENCES URLList(rowId),
                                     FOREIGN KEY (fk_ToURLId) REFERENCES URLList(rowId)
-                ); """
+                ); '''
             )
-        cursor.execute("""CREATE TABLE IF NOT EXISTS linkWord  (
+        cursor.execute('''CREATE TABLE IF NOT EXISTS linkWord  (
                                     rowid INTEGER PRIMARY KEY AUTOINCREMENT,
                                     fk_wordId INTEGER NOT NULL,
                                     fk_linkId INTEGER NOT NULL,
                                     FOREIGN KEY (fk_wordId) REFERENCES wordList(rowId),
                                     FOREIGN KEY (fk_linkId) REFERENCES linkBetweenURL(rowId)
-                ); """
+                ); '''
             )
 
     # Вспомогательная функция для получения идентификатора записи
@@ -96,12 +117,13 @@ class Crawler:
         cursor = self.conn.cursor()
         cursor.execute('INSERT INTO linkBetweenURL VALUES (?, ?, ?);', 
                        (None, urlFrom, urlTo))
-        last_entry = cursor.lastrowid()
+        last_entry = cursor.lastrowid
         linkwords_list = self.separateWords(linkText)
         for _word_ in linkwords_list:
-            word_rowid = self.getEntryId('wordList', 'rowid', _word_)
-            cursor.execute('INSERT INTO linkWord VALUES (?, ?, ?);',
-                           (None, word_rowid, last_entry))
+            word_rowid = self.getEntryId('wordList', 'word', _word_)
+            if word_rowid:
+                cursor.execute('INSERT INTO linkWord VALUES (?, ?, ?);',
+                               (None, word_rowid, last_entry))
             
     def filteredLinks(self, links_list, sourceURL):
         cursor = self.conn.cursor()
@@ -110,13 +132,13 @@ class Crawler:
             # текст в ссылке
             _a_tag_text = link.get_text().strip()
             new_link = link.get('href').rstrip('/')
-            if "#" in new_link: continue
+            if '#' in new_link: continue
             if new_link.startswith('/'): 
                 new_link = urljoin(sourceURL, new_link)
             if self.isIndexed(new_link): continue
 
             cursor.execute('INSERT INTO URLList VALUES (?, ?);', (None, new_link))
-            self.addLinkRef(link, new_link, _a_tag_text)
+            self.addLinkRef(sourceURL, new_link, _a_tag_text)
             new_URLs.append(new_link)
         return new_URLs
 
@@ -129,25 +151,30 @@ class Crawler:
 
         # Вставка первых двух ссылок
         for url_ in urlList:
-            cursor.execute("""INSERT INTO URLList (URL) VALUES (?);""", (url_,))
+            cursor.execute('INSERT INTO URLList (URL) VALUES (?);', (url_,))
 
         for _ in range(0, maxDepth):
             for url_ in urlList:
-                html_doc = requests.get(url_).text
-                soup = BeautifulSoup(html_doc, "html.parser")
+                print(url_)
+                html_doc = requests.get(url_)
+                html_doc.encoding = 'utf-8'
+                soup = BeautifulSoup(html_doc.text, 'html.parser')
 
-                # Кусок со ссылками, надо перенести в конец
+                self.addToIndex(soup, url_)
+
                 all_links = soup.find_all('a')
-                self.filteredLinks(all_links. url_)
-
-
+                urlList = self.filteredLinks(all_links, url_)
+                
+                self.conn.commit()
 
 
 if __name__ == '__main__':
+
     crawler = Crawler('DB.db')
     # links = ['https://history.eco']
-    links = ['https://www.reddit.com/?rdt=35077']
+    # links = ['https://www.reddit.com/?rdt=35077']
     # links = ['https://history.eco/', 'https://elementy.ru/']
 
-    crawler.crawl(links, 1)
-    # print(crawler.getEntryId('wordList', 'word', 'skip'))
+    links = ['http://127.0.0.1:8080/1_leguria.html']
+
+    crawler.crawl(links, 2)
